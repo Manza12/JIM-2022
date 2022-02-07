04a8d1541e16cdc679cfbbc5a0a632df157ad04a
Manza12 <tritery@hotmail.com>
2022-02-07 16:42:32 +0100

    Remove old stuff

--- old/run.py
+++
@@ -1,7 +0,0 @@
-from tqdm import tqdm
-from extraction import extract_harmonic, extract_inharmonic
-
-for i in tqdm(range(21, 109)):
-    name = str(i)
-    extract_harmonic(name)
-    extract_inharmonic(name)
