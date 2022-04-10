af5d09ffebe764d3aad7e3fc13b599eeb4b04f8f
Manza12 <tritery@hotmail.com>
2022-04-10 13:51:14 +0200

    Put figure size as a visible parameter

--- instruments/violin/55/plot_figures.py
+++ instruments/violin/55/plot_figures.py
@@ -2,6 +2,9 @@ from plot import plot_figures
 from pathlib import Path
 import matplotlib.pyplot as plt
 
+
+fig_size = (640, 360)
+
 if __name__ == '__main__':
-    plot_figures(Path('.'))
+    plot_figures(Path('.'), fig_size)
     plt.show()
