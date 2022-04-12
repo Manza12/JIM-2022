b51d545f9d27787ff30d7f271041c43a77ad0ef4
Manza12 <tritery@hotmail.com>
2022-04-12 23:03:50 +0200

    Change skeleton to the original definition

--- skeleton.py
+++ skeleton.py
@@ -2,13 +2,17 @@ import numpy as np
 import scipy.ndimage.morphology as morpho
 
 
-def skeleton(image_input, structuring_element):
+def skeleton(image_input):
+    str_el_opening = np.ones((2, 1), dtype=bool)
     result = np.zeros_like(image_input).astype(bool)
     tmp = np.copy(image_input).astype(bool)
+    n = 1
     while np.any(tmp):
-        opening = morpho.binary_opening(tmp, structure=structuring_element)
+        str_el_erosion = np.ones((n, 1), dtype=bool)
+        opening = morpho.binary_opening(tmp, structure=str_el_opening)
         top_hat = np.logical_and(tmp, np.logical_not(opening))
         result = np.logical_or(result, top_hat)
-        tmp = morpho.binary_erosion(tmp, structure=structuring_element)
+        tmp = morpho.binary_erosion(image_input, structure=str_el_erosion)
+        n += 1
 
     return result.astype(np.int8)
